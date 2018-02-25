55f98b2ee5227bd5b85dc59ca0fbbe7f111fd0ff
lombao <cesar.lombao@gmail.com>
2018-02-25 18:50:39 +0000

    Added conky and few things more

--- CORE/bzip2/POSTLOAD/usr/etc/profile.d/bzip2.sh
+++ CORE/bzip2/POSTLOAD/usr/etc/profile.d/bzip2.sh
@@ -3,3 +3,4 @@ pathappend %PREFIX/include	C_INCLUDE_PATH
 pathappend %PREFIX/include	CPLUS_INCLUDE_PATH
 pathappend %PREFIX/lib		LIBRARY_PATH
 pathappend %PREFIX/share/man	MANPATH
+pathappend %PREFIX		CMAKE_PREFIX_PATH
--- CORE/ncurses/POSTLOAD/usr/etc/profile.d/ncurses.sh
+++ CORE/ncurses/POSTLOAD/usr/etc/profile.d/ncurses.sh
@@ -4,4 +4,6 @@ pathappend	%PREFIX/include/ncursesw	C_INCLUDE_PATH
 pathappend	%PREFIX/bin			PATH
 pathappend	%PREFIX/lib/pgkconfig		PKG_CONFIG_PATH
 pathappend	%PREFIX/share/man		MANPATH
+pathappend	%PREFIX				CMAKE_PREFIX_PATH
+
 export TERMINFO=%PREFIX/share/terminfo
