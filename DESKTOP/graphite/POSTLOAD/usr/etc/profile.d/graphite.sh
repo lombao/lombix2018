ad1b808424b107152f4108b76947c52044207a9e
lombao <cesar.lombao@gmail.com>
2018-03-20 17:43:33 +0000

    Added ninja, upgrade firefox, fixes in docbook, and more

---
+++ DESKTOP/graphite/POSTLOAD/usr/etc/profile.d/graphite.sh
@@ -0,0 +1,3 @@
+pathappend	%PREFIX/lib/pkgconfig	PKG_CONFIG_PATH
+pathappend	%PREFIX/bin		PATH
+
---
+++ XML/libxml2/POSTLOAD/usr/etc/profile.d/xml.sh
@@ -0,0 +1,8 @@
+pathappend	%PREFIX/bin				PATH
+pathappend      %PREFIX/lib/python3.6/site-packages    PYTHONPATH
+pathappend      %PREFIX/lib/python2.7/site-packages    PYTHONPATH
+pathappend	%PREFIX/lib/pkgconfig		PKG_CONFIG_PATH
+pathappend	%PREFIX/share/man		MANPATH
+pathappend	%PREFIX/share/aclocal		ACLOCAL_PATH
+pathappend	%PREFIX				CMAKE_PREFIX_PATH
+
