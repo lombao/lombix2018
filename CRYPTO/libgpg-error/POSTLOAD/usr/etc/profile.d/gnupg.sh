8e158f075e810e29ff40b6eab5fcdffbea0c3c35
lombao <cesar.lombao@gmail.com>
2018-03-23 16:16:41 +0000

    More updates , fixed openssl/openssh error

--- CRYPTO/libgpg-error/POSTLOAD/usr/etc/profile.d/gnupg.sh
+++ CRYPTO/libgpg-error/POSTLOAD/usr/etc/profile.d/gnupg.sh
@@ -1,5 +1,7 @@
 pathappend      %PREFIX/bin		PATH
 pathappend      %PREFIX/lib		LIBRARY_PATH
 pathappend      %PREFIX/include		C_INCLUDE_PATH
+pathappend	%PREFIX/include		CPLUS_INCLUDE_PATH
 pathappend	%PREFIX/share/man	MANPATH
 pathappend	%PREFIX/share/aclocal	ACLOCAL_PATH
+pathappend	%PREFIX			CMAKE_PREFIX_PATH
