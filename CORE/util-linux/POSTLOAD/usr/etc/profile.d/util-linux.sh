5be8b99ccf7eb16371f106f5e4c70f1b2241e35e
lombao <cesar.lombao@gmail.com>
2018-04-01 10:59:04 +0000

    Fix stupid error for util linux pkgconfig package, which made the blkid not available for eudev

--- CORE/util-linux/POSTLOAD/usr/etc/profile.d/util-linux.sh
+++ CORE/util-linux/POSTLOAD/usr/etc/profile.d/util-linux.sh
@@ -2,6 +2,7 @@ pathappend %PREFIX/lib		LIBRARY_PATH
 pathappend %PREFIX/include	C_INCLUDE_PATH
 pathappend %PREFIX/bin		PATH
 pathappend %PREFIX/share/man	MANPATH
+pathappend %PREFIX/lib/pkgconfig PKG_CONFIG_PATH
 
 
 
