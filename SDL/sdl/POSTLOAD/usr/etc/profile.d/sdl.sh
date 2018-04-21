e2a175d4a57b9b55ecef0d0c8c687b34bef173c7
lombao <cesar.lombao@gmail.com>
2018-04-21 19:55:57 +0000

    Fixed the issue with ffmpeg and mplayer

--- SDL/sdl/POSTLOAD/usr/etc/profile.d/sdl.sh
+++ SDL/sdl/POSTLOAD/usr/etc/profile.d/sdl.sh
@@ -2,3 +2,5 @@ pathappend	%PREFIX/bin		PATH
 pathappend	%PREFIX/share/aclocal	ACLOCAL_PATH
 pathappend	%PREFIX/share/man	MANPATH
 pathappend	%PREFIX/lib/pkgconfig	PKG_CONFIG_PATH
+pathappend	%PREFIX/include		C_INCLUDE_PATH
+pathappend	%PREFIX/lib		LIBRARY_PATH
